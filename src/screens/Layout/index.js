df459a738622485d7fc9380528ec553a140ce998
devJSFullStack <dev.fullstack.js0311@gmail.com>
2020-07-20 16:08:33 +0300

    ff

--- src/screens/Layout/index.js
+++ src/screens/Layout/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {
-  View,
-  Text,
-  TextInput,
-  Button
-} from 'react-native';
+import {View} from 'react-native';
 import AppHeader from '../../components/Headers';
 
 const styles = StyleSheet.create({
