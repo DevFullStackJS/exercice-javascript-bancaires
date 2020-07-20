ff605fd2721c8c4c3e8d6746a1e2549a893cee72
devJSFullStack <dev.fullstack.js0311@gmail.com>
2020-07-20 13:10:58 +0300

    Fix server

--- scripts/server/index.js
+++ scripts/server/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const routes = require("./routes");
 
-express()
-  .use(express.static(path.join(__dirname, '../../build')))
+// App
+const app = express();
+
+// Set port
+const port = process.env.PORT || 5000;
+app.set("port", port);
+
+app.use(express.static(path.join(__dirname, '../../build')))
   .set('static', path.join(__dirname, 'static'))
-  // .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('../../build/index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use('/api', routes);
+  
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
+
+
+// express()
+//   .use(express.static(path.join(__dirname, '../../build')))
+//   .set('static', path.join(__dirname, 'static'))
+//   // .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('../../build/index.html'))
+
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
