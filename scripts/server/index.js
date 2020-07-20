f189936b3d77ec27ecb110df6799aa48094c6af5
devJSFullStack <dev.fullstack.js0311@gmail.com>
2020-07-20 16:32:20 +0300

    Add server mongo

--- scripts/server/index.js
+++ scripts/server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const routes = require("./routes");
+const mongoose = require("mongoose");
 
 // App
 const app = express();
@@ -8,6 +9,16 @@ const app = express();
 // Set port
 const port = process.env.PORT || 5000;
 app.set("port", port);
+
+const urlMongo = 'mongodb+srv://7Z0tB7RV4yVuG6fPm7m:7Z0tB7RV4yVuG6fPm7m@cluster0.ddlxp.azure.mongodb.net/nodejsauth';
+mongoose
+  .connect(urlMongo, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.use(express.static(path.join(__dirname, '../../build')))
   .set('static', path.join(__dirname, 'static'))
