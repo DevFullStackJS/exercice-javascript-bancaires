3ac8525637407e93a1a6a75ed17370fe97136c83
lahnjara <dev.fullstack.js0311@gmail.com>
2020-07-20 10:43:04 +0300

    First commit

---
+++ scripts/server/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const todos = require("./todos");
+
+const router = express.Router();
+
+router.get("/todos", function(req, res) {
+  res.json(todos);
+});
+
+router.post("/login", function(req, res) {
+  res.json(todos);
+});
+
+module.exports = router;
