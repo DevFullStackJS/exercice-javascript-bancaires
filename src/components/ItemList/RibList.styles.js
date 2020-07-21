a8f27d0be0ac6b4536c1bde69fc31d3565f47786
DevFullStackJS <dev.fullstack.js0311@gmail.com>
2020-07-21 11:20:15 +0200

    List show style

---
+++ src/components/ItemList/RibList.styles.js
@@ -0,0 +1,18 @@
+export const styles = {
+  headList: {
+    flexDirection: 'row',
+    height: 100,
+    padding: 10,
+    backgroundColor: 'beige',
+    // backgroundColor: 'red',
+  },
+  titleView: {
+    flex: 0.2,
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+  separate: {
+    padding: 10,
+  },
+};
---
+++ src/screens/Operation/index.styles.js
@@ -0,0 +1,29 @@
+export const styles = {
+  headList: {
+    flexDirection: 'row',
+    height: 100,
+    padding: 20,
+    backgroundColor: 'grey',
+  },
+  footTotal: {
+    height: 100,
+    padding: 20,
+    flexDirection: 'row',
+    backgroundColor: 'grey',
+  },
+  showTotal: {
+    flex: 0.5,
+  },
+  titleView: {
+    flex: 0.2,
+  },
+  titleText: {
+    fontWeight: 'bold',
+  },
+  ribCode: {
+    flexDirection: 'row',
+    backgroundColor: 'grey',
+    paddingTop: 10,
+    paddingLeft: 5,
+  },
+};
