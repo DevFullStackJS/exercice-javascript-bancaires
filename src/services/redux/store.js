4ca7e2b055e4d82f8d5451dc6228a7303b5811f5
devJSFullStack <dev.fullstack.js0311@gmail.com>
2020-07-21 06:40:36 +0300

    Fix Mobile

--- src/services/redux/store.js
+++ src/services/redux/store.js
@@ -1,28 +1,50 @@
+// import { applyMiddleware, createStore, compose } from 'redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+
+// import thunk from 'redux-thunk';
+// import { Platform } from 'react-native';
+// // import storage from 'redux-persist/lib/storage';
+// import storage from '@react-native-community/async-storage';
+// import rootReducer from './reducers';
+
+// // Store In the previous sections, we defined the actions that represent the facts about "what happened"
+// // and the reducers that update the state according to those actions.
+// // The Store is the object that brings them together.
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['navigation'],
+// };
+
+// const pReducer = persistReducer(persistConfig, rootReducer);
+
+// const store = createStore(
+//   pReducer,
+//   compose(applyMiddleware(thunk)),
+// );
+
+// export const persistor = persistStore(store);
+
+// export default store;
+
 import { applyMiddleware, createStore, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import thunk from 'redux-thunk';
-import { Platform } from 'react-native';
-import storageWeb from 'redux-persist/lib/storage';
-import storageMobile from '@react-native-community/async-storage';
-import rootReducer from './reducers';
+import storage from '@react-native-community/async-storage';
 
-// Store In the previous sections, we defined the actions that represent the facts about "what happened"
-// and the reducers that update the state according to those actions.
-// The Store is the object that brings them together.
+import rootReducer from './reducers';
 
 const persistConfig = {
   key: 'root',
-  storage: Platform.OS === 'web' ? storageWeb : storageMobile,
-  blacklist: ['navigation'],
+  storage,
+  stateReconciler: autoMergeLevel2,
 };
 
 const pReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(
-  pReducer,
-  compose(applyMiddleware(thunk)),
-);
+const store = createStore(pReducer, compose(applyMiddleware(thunk)));
 
 export const persistor = persistStore(store);
 
