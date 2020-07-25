4ca78fe8acb23d7541fe2d218020cbf2c13cf756
DevFullStackJS <dev.fullstack.js0311@gmail.com>
2020-07-25 17:23:26 +0200

    home french traduction

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,13 @@
 module.exports = {
+  home_title: 'Compagnie fiduciaire',
   error_mdp: 'Le mot de passe est incorrect.',
   error_email: 'Identifiants est incorrect.',
   not_found: 'User not found',
   invalid_date: 'Invalid date format',
   invalid_rib: 'Invalid rib',
+  select_period: 'Choisir la période',
+  from: 'Du',
+  to: 'Au',
+  select_rib: 'Choisir RIB',
+  find: 'Rechercher',
 };
